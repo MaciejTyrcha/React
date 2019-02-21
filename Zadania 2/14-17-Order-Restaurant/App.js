9832f434cf2779000984ac396fb61ad7292849d9
aertuss <maciej.tyrcha@o2.pl>
2019-02-21 16:55:43 +0100

    restauracja koszyk

---
+++ Zadania 2/14-17-Order-Restaurant/App.js
@@ -0,0 +1,35 @@
+class App extends React.Component {
+  state = { 
+    items: [
+      {id: 1, name: "herbata", active: true},
+      {id: 2, name: "ziemniaki", active: false},
+      {id: 3, name: "kasza", active: false},
+      {id: 4, name: "zupa", active: true},
+      {id: 5, name: "woda", active: false},
+      {id: 6, name: "chleb", active: false},
+    ],
+   }
+
+   handleChangeStatus = (id) => {
+     const itemList = this.state.items.map(item => {
+       if (id === item.id) {
+         item.active = !item.active;
+       }
+       return item;
+     });
+
+     this.setState({
+       items: itemList
+     })
+   }
+
+  render() { 
+    return (  
+      <>
+        <Header items = {this.state.items}/>
+        <ListItems items = {this.state.items} changeStatus = {this.handleChangeStatus}/>
+      </>
+    );
+  }
+}
+ 
